6164b4f6cd9a6523321c5467e3944350227185d7
human <human@humans-MacBook-Pro.local>
2020-09-30 18:33:04 +0900

    :hammer: add files to study team dev. and ios app dev.
    岡野くんと近藤くんの勉強用ファイルを追加

---
+++ winnerlights-ios-mobile-client/ViewControllers/CircularProgressViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CircularProgressViewController.swift
+//  winnerlights-ios-mobile-client
+//
+//  Created by human on 2020/09/30.
+//
+
+import UIKit
+
+class CircularProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ winnerlights-ios-mobile-client/ViewControllers/SeekBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SeekBarViewController.swift
+//  winnerlights-ios-mobile-client
+//
+//  Created by human on 2020/09/30.
+//
+
+import UIKit
+
+class SeekBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        self.title = "SeekBar"
+    }
+}
