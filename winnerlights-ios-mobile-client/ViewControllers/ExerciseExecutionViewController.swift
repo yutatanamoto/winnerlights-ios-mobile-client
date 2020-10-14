f77f1af6057af74e25e5561280bc0794c192ccb2
yutatanamoto <yutatanamoto@humans-MacBook-Pro.local>
2020-10-14 11:26:23 +0900

    :fire: Remove unnecessary semicolon

--- winnerlights-ios-mobile-client/ViewControllers/ExerciseExecutionViewController.swift
+++ winnerlights-ios-mobile-client/ViewControllers/ExerciseExecutionViewController.swift
@@ -360,7 +360,7 @@ class PartitionBarGroupView: UIView {
         ]
     )
     override init(frame: CGRect) {
-        super.init(frame: frame);
+        super.init(frame: frame)
     }
     
     required init?(coder aDecoder: NSCoder) {
