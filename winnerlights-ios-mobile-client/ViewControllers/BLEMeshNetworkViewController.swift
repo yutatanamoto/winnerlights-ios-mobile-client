6f6e5e94c2edb5e0b0e391f3f4fb0ded3b2d3dd1
yutatanamoto <yutatanamoto@humans-MacBook-Pro.local>
2020-10-15 22:01:51 +0900

    :poop: Mess

--- winnerlights-ios-mobile-client/ViewControllers/BLEMeshNetworkViewController.swift
+++ winnerlights-ios-mobile-client/ViewControllers/BLEMeshNetworkViewController.swift
@@ -8,10 +8,153 @@
 import UIKit
 
 class BLEMeshNetworkViewController: UIViewController {
+    let cornerRadius: CGFloat = 20
+    let shadowOpacity: Float = 0.2
+    let marginWidth: CGFloat = 16
+    let shadowOffset: CGSize = CGSize(width: 4, height: 4)
+    
+    fileprivate lazy var addButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .white
+        button.layer.cornerRadius = cornerRadius
+        button.layer.shadowOpacity = shadowOpacity
+        button.layer.shadowRadius = cornerRadius
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = shadowOffset
+        button.setTitle("Add Node", for: .normal)
+        button.addTarget(self, action: #selector(showAvairablePeripherals), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
         self.title = "BLE Mesh Network"
+        
+        view.addSubview(addButton)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        addButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -marginWidth).isActive = true
+        addButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -marginWidth).isActive = true
+        addButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        addButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    @objc func showAvairablePeripherals() {
+        let vc = AvairablePeripheralViewController()
+        self.navigationController?.present(vc, animated: true)
     }
  }
+
+
+import CoreBluetooth
+import nRFMeshProvision
+
+typealias DiscoveredPeripheral = (
+    device: UnprovisionedDevice,
+    peripheral: CBPeripheral,
+    rssi: Int
+)
+class AvairablePeripheralViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager!
+    fileprivate lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        self.title = "BLE Mesh Network"
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
+        
+        view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "YoYo"
+        return cell
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOn:
+            print("CBManager is powered on")
+            startScan()
+        case .poweredOff:
+            print("CBManager is not powered on")
+            return
+        case .resetting:
+            print("CBManager is resetting")
+        case .unauthorized:
+            print("Unexpected authorization")
+            return
+        case .unknown:
+            print("CBManager state is unknown")
+            return
+        case .unsupported:
+            print("Bluetooth is not supported on this device")
+            return
+        @unknown default:
+            print("A previously unknown central manager state occurred")
+            return
+        }
+    }
+    
+    func startScan(){
+        print("begin to scan ...")
+        centralManager.scanForPeripherals(withServices: nil)
+    }
+    
+    func stopScan() {
+        self.centralManager.stopScan()
+    }
+
+    // Peripheral探索結果を処理
+    func centralManager(_ central: CBCentralManager,
+                        didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String: Any],
+                        rssi RSSI: NSNumber) {
+
+        print("pheripheral.name: \(String(describing: peripheral.name))")
+        print("advertisementData:\(advertisementData)")
+        print("RSSI: \(RSSI)")
+        print("peripheral.identifier.uuidString: \(peripheral.identifier.uuidString)\n")
+        
+        // check whether discovered peripheral is contained by discoveredPeripherals
+//        if let index = discoveredPeripherals.firstIndex(where: { $0.peripheral == peripheral }) {
+//            print("no new peripheral discovered", index)
+//        } else {
+//            if let unprovisionedDevice = UnprovisionedDevice(advertisementData: advertisementData) {
+//                discoveredPeripherals.append((unprovisionedDevice, peripheral, RSSI.intValue))
+//                tableView.insertRows(at: [IndexPath(row: discoveredPeripherals.count - 1, section: 0)], with: .fade)
+//            }
+//        }
+    }
+}
