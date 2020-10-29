355c70a505eeb054f5c591811ac7e9e3dae2e9f9
human <human@hyumannoMacBook-Air.local>
2020-10-30 00:25:29 +0900

    円形プログレスバーのデジタル時間表示実装
    シークバーの時間表示とのズレがあるので修正必要

--- winnerlights-ios-mobile-client/ViewControllers/ExerciseExecutionViewController.swift
+++ winnerlights-ios-mobile-client/ViewControllers/ExerciseExecutionViewController.swift
@@ -37,7 +37,7 @@ class ExerciseExecutionViewController: UIViewController {
         description: "Basic exercise. There are 2 goals and 4 players on each team.",
         phases: [
             Phase(
-                duration: 10,
+                duration: 65,
                 goals: [
                     Goal(position: .upperLeft, color: .pink),
                     Goal(position: .lowerLeft, color: .pink),
@@ -409,8 +409,17 @@ class CircularProgressView: UIView {
     
     func updateProgress(currentPahseTime: Float, currentPhaseProgress: Float) {
         progressLayer.strokeEnd = CGFloat(currentPhaseProgress)
-        let formattedCurrentPahseTime = String(format:"%.0f", currentPahseTime)
-        progressLabel.text = "\(formattedCurrentPahseTime)"
+        let formattedCurrentPahseTimeMinute = String(format:"%.0f", (currentPahseTime/60.0).rounded(.towardZero))
+        let formattedCurrentPahseTimeSecond = String(format:"%02.0f", ceil(currentPahseTime.truncatingRemainder(dividingBy: 60.0)))
+        if  currentPahseTime > 60.0{
+        progressLabel.text = "\(formattedCurrentPahseTimeMinute):\(formattedCurrentPahseTimeSecond)"
+        } else if ceil(currentPahseTime.truncatingRemainder(dividingBy: 60.0)) == 60.0 {
+        let formattedCurrentPahseTimeSpecialMinute = String(format:"%.0f", (currentPahseTime/60.0).rounded(.towardZero)+1)
+        progressLabel.text = "\(formattedCurrentPahseTimeSpecialMinute):00"
+        }
+        else {
+        progressLabel.text = "\(formattedCurrentPahseTimeSecond)"
+        }
     }
 }
 
